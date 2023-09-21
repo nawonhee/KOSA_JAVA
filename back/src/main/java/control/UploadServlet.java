61abbfa3ba8c51f28afd420784cb4e0b5d248722
nawonhee <nwh0921@naver.com>
2023-09-21 10:04:14 +0900

    라이브러리 설치

--- back/src/main/java/control/UploadServlet.java
+++ back/src/main/java/control/UploadServlet.java
@@ -1,6 +1,8 @@
 package control;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.Scanner;
 
 import javax.servlet.ServletException;
@@ -10,6 +12,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.tomcat.util.http.fileupload.FileItem;
+import org.apache.tomcat.util.http.fileupload.FileUploadException;
+import org.apache.tomcat.util.http.fileupload.RequestContext;
+import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
 import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
 
 /**
@@ -32,7 +38,27 @@ public class UploadServlet extends HttpServlet {
 //		}
 		
 		String attachesDir = "D:\\KOSA202307\\attaches"; //첨부경로
-		ServletFileUpload fileUpload = new ServletFileUpload();
+		DiskFileItemFactory fileItemFactory = new DiskFileItemFactory();
+		File repository = new File(attachesDir);
+		fileItemFactory.setRepository(repository);
+		
+		ServletFileUpload fileUpload = new ServletFileUpload(fileItemFactory);
+		
+		try{
+			List<FileItem> items = fileUpload.parseRequest(request);
+			for(FileItem item : items) {
+				if(item.isFormField()) { //요청전달데이터인 경우
+					System.out.println(item.getFieldName()+":"+item.getString());
+				}else { //첨부파일인 경우
+					System.out.println(item.getName()+":"+item.getSize());
+					if(item.getSize()>0) {
+						
+					}
+				}
+			}
+		}catch(FileUploadException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
