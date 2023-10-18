25e9d7635dcecc924090c6ad8bb93869a4972890
nawonhee <nwh0921@naver.com>
2023-10-18 16:16:17 +0900

    2023.10.17.

---
+++ back2/src/main/java/com/my/listner/MyServletContextListener.java
@@ -0,0 +1,35 @@
+package com.my.listner;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Application Lifecycle Listener implementation class MyServletContextListener
+ *
+ */
+@WebListener
+public class MyServletContextListener implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public MyServletContextListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent sce)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent sce)  { 
+         System.out.println("MyServletContextListener.contextInitialized()호출됨");
+    }
+	
+}
