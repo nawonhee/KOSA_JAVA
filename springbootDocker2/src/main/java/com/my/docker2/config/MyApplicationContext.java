3d0cafab2c9a53711d9198aba55a4bfc92c0729e
nawonhee <nwh0921@naver.com>
2023-11-06 17:20:57 +0900

    2023.11.06.

---
+++ springbootDocker2/src/main/java/com/my/docker2/config/MyApplicationContext.java
@@ -0,0 +1,50 @@
+package com.my.docker2.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+@EnableTransactionManagement
+@Slf4j
+@PropertySource("classpath:/db.properties")
+public class MyApplicationContext {
+	@Value("${spring.datasource.hikari.driver-class-name}")
+	private String hikariDriverClassName;
+	
+	@Autowired
+	Environment env;
+
+	@Bean
+	@ConfigurationProperties("spring.datasource.hikari")
+	public HikariConfig hikariConfig() {
+		HikariConfig config = new HikariConfig();
+		//config.setUsername();
+		//config.setPassword();
+		return config;
+	}
+	
+	@Bean
+	public DataSource dataSourceHikari() {
+		return new HikariDataSource(hikariConfig());
+	}
+	
+}
